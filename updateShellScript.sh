5b45133ccab8ea5d73f037648bc5a531355de993
IamOsatoEdosa <98400676+IamOsatoEdosa@users.noreply.github.com>
2022-06-11 23:40:42 +0100

    Add files via upload

---
+++ updateShellScript.sh
@@ -0,0 +1,2 @@
+aws cloudformation update-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --region=us-east-1 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
+#This file contains the update-stack command, which expects three command-line arguments.
